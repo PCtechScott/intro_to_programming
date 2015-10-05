346b5648512440ecc13c5e8c2a3cc88437c8408d
Scott Grossman <pctechscott@gmail.com>
2015-10-05 18:34:26 -0400

    Added files chapter.

---
+++ files/slashdot_parse.rb
@@ -0,0 +1,17 @@
+# slashdot_parse.rb
+
+slashdot_articles = []
+File.open("slashdot.xml", "r") do |f|
+  doc = Nokogiri::XML(f)
+  slashdot_articles = doc.css('item').map { |i|
+    {
+      title: i.at_css('title').content,
+      link: i.at_css('link').content,
+      summary: i.at_css('description').content
+    }
+  }
+end
+
+# Yup, I choked on this one too, even though it was pasted directly
+# from the ebook. My nokogiri and axlsx installations both appeared to
+# have succeeded after some initial hiccups, so I dunno what's up. :(
